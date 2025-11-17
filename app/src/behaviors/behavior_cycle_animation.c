eb97b7e43049d3ae501780f9aef510195008efa0
Honeycat <andrew.tsakas@gmail.com>
2025-11-18 08:23:00 +1000

    Attempt #2

--- app/src/behaviors/behavior_cycle_animation.c
+++ app/src/behaviors/behavior_cycle_animation.c
@@ -9,32 +9,59 @@
 #include <zephyr/device.h>
 #include <drivers/behavior.h>
 #include <zephyr/logging/log.h>
+#include <zmk/behavior.h>
+
 #include <zmk/keymap.h>
-#include "boards/shields/nice_view_gem/widgets/animation.h"
+#include <zmk/keys.h>
+
+#include <zmk/endpoints.h>
+#include <zmk/event_manager.h>
+
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
-static const struct behavior_parameter_value_metadata no_arg_values[] = {
-    { .display_name = "Toggle Animation", .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE, .value = NVC_TOG },
-    { .display_name = "Turn Animation Off", .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE, .value = NVC_OFF },
-    { .display_name = "Turn Animation On",  .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE, .value = NVC_ON  },
-    { .display_name = "Cycle Theme",       .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE, .value = NVC_NEXT },
+
+static const struct behavior_parameter_value_metadata param_values[] = {
+    {
+        .display_name = "Toggle Animation Movement",
+        .value = NVC_TOG,
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+    },
+    {
+        .display_name = "Turn Off Animation Movement",
+        .value = NVC_OFF,
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+    },
+    {
+        .display_name = "Turn On Animation Movement",
+        .value = NVC_ON,
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+    },
+    {
+        .display_name = "Go to next Theme",
+        .value = NVC_NEXT,
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+    },
 };
+
+static const struct behavior_parameter_metadata_set param_metadata_set[] = {{
+    .param1_values = param_values,
+    .param1_values_len = ARRAY_SIZE(param_values),
+}};
 
 static const struct behavior_parameter_metadata metadata = {
-    .sets = no_arg_values,
-    .sets_len = ARRAY_SIZE(no_arg_values),
+    .sets_len = ARRAY_SIZE(param_metadata_set),
+    .sets = param_metadata_set,
 };
-#endif
 
+#endif
 
 static int on_keymap_binding_convert_central_state_dependent_params(
     struct zmk_behavior_binding *binding,
-    struct zmk_behavior_binding_event event
-) {
+    struct zmk_behavior_binding_event event) {
     if (binding->param1 == NVC_TOG) {
         binding->param1 = nice_view_animation_is_enabled() ? NVC_OFF : NVC_ON;
     }
@@ -42,10 +69,8 @@ static int on_keymap_binding_convert_central_state_dependent_params(
 }
 
 
-static int on_keymap_binding_pressed(
-    struct zmk_behavior_binding *binding,
-    struct zmk_behavior_binding_event event
-) {
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                    struct zmk_behavior_binding_event event) {
     switch (binding->param1) {
     case NVC_TOG:
         nice_view_animation_toggle();
@@ -62,29 +87,21 @@ static int on_keymap_binding_pressed(
     case NVC_NEXT:
         nice_view_theme_next();
         return ZMK_BEHAVIOR_OPAQUE;
-
     default:
         LOG_ERR("Unknown animation command param1=%d", binding->param1);
-        return -ENOTSUP;
     }
+        return -ENOTSUP;
 }
 
-
-static int on_keymap_binding_released(
-    struct zmk_behavior_binding *binding,
-    struct zmk_behavior_binding_event event
-) {
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding, 
+                                    struct zmk_behavior_binding_event event) {
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
-
 static const struct behavior_driver_api behavior_cycle_animation_driver_api = {
-    .binding_convert_central_state_dependent_params =
-        on_keymap_binding_convert_central_state_dependent_params,
-    .binding_pressed =
-        on_keymap_binding_pressed,
-    .binding_released =
-        on_keymap_binding_released,
+    .binding_convert_central_state_dependent_params = on_keymap_binding_convert_central_state_dependent_params,
+    .binding_pressed =  on_keymap_binding_pressed,
+    .binding_released = on_keymap_binding_released,
     .locality = BEHAVIOR_LOCALITY_GLOBAL,
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
     .parameter_metadata = &metadata,
@@ -92,7 +109,9 @@ static const struct behavior_driver_api behavior_cycle_animation_driver_api = {
 };
 
 BEHAVIOR_DT_INST_DEFINE(
-    0, NULL, NULL, NULL, NULL,
+    0, 
+    NULL, NULL, 
+    NULL, NULL,
     APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
     &behavior_cycle_animation_driver_api
 );
